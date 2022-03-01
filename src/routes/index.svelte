db51db8b3690dce30c9be0fd11d8dd94d881b885
Lisa <halvorsen.lisa@gmail.com>
2022-03-01 20:56:39 +0100

    Form in html

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,23 +2,23 @@
 	import { language } from '$lib/stores/language';
 	export const prerender = true;
 
-	/** @type {import('@sveltejs/kit').Load} */
-	export async function load({ params, fetch, session, stuff }) {
-		if (params?.lang === 'en') {
-			language.set('en');
-			return {
-				status: 301,
-				redirect: '/en'
-			};
-		}
-		return {
-			status: 301,
-			redirect: '/no/'
-		};
-	}
+	// /** @type {import('@sveltejs/kit').Load} */
+	// export async function load({ params, fetch, session, stuff }) {
+	// 	if (params?.lang === 'en') {
+	// 		language.set('en');
+	// 		return {
+	// 			status: 301,
+	// 			redirect: '/en'
+	// 		};
+	// 	}
+	// 	return {
+	// 		status: 301,
+	// 		redirect: '/no/'
+	// 	};
+	// }
 </script>
 
-<form name="test" method="POST" netlify netlify-honeypot="bot-field">
+<form name="test" method="POST" data-netlify="true" netlify-honeypot="bot-field">
 	<label for="name">Name</label>
 	<input id="name" name="name" type="text" />
 	<button type="submit">Send inn</button>

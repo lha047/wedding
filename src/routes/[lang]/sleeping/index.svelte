cf5e31445989d47533e763ef1d6955c6b578beb9
Lisa <halvorsen.lisa@gmail.com>
2022-03-01 16:04:31 +0100

    Add test form

--- src/routes/[lang]/sleeping/index.svelte
+++ src/routes/[lang]/sleeping/index.svelte
@@ -5,10 +5,14 @@
 <h1>Overnatting</h1>
 
 <!-- <RegistrationForm /> -->
+<form name="test-form" method="POST" data-netlify="true">
+	<label for="name">Name</label>
+	<input id="name" name="name" type="text" />
+	<button type="submit">Send inn</button>
+</form>
 
 <style lang="scss">
 	h1 {
 		text-align: center;
 	}
-	
 </style>

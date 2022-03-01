9e1d90298ad78130b1f99decadef54728dbb41f6
Lisa <halvorsen.lisa@gmail.com>
2022-03-01 20:36:52 +0100

    Form netlify (#5)
    * Add test form
    
    * Add form

--- src/routes/[lang]/sleeping/index.svelte
+++ src/routes/[lang]/sleeping/index.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
 <script lang="ts">
 	import RegistrationForm from '../../../components/RegisterForm.svelte';
 </script>
@@ -5,10 +9,14 @@
 <h1>Overnatting</h1>
 
 <!-- <RegistrationForm /> -->
+<form name="test" method="POST" netlify netlify-honeypot="bot-field">
+	<label for="name">Name</label>
+	<input id="name" name="name" type="text" />
+	<button type="submit">Send inn</button>
+</form>
 
 <style lang="scss">
 	h1 {
 		text-align: center;
 	}
-	
 </style>

c6aa563a6055bf372e0bf21c6536f68e98e35832
Lisa <halvorsen.lisa@gmail.com>
2022-03-06 18:56:02 +0100

    Add getting info from localstorage

--- src/lib/stores/auth.ts
+++ src/lib/stores/auth.ts
@@ -1,20 +1,23 @@
 import { writable } from 'svelte/store'
+import { browser } from '$app/env'
 
-export const isLoggedIn = writable(false)
+const login = browser ? localStorage.getItem('gotrue.user') : null
+
+export const isLoggedIn = writable(login)
 
 export function addListener() {
 	netlifyIdentity.on('init', (user) => {
 		console.log('on init', user)
 		if (user) {
-			isLoggedIn.set(true)
+			isLoggedIn.set(user)
 		}
 	})
 	netlifyIdentity.on('login', (user) => {
 		console.log('on log in ', user)
-		isLoggedIn.set(true)
+		isLoggedIn.set(user)
 	})
 	netlifyIdentity.on('logout', (user) => {
 		console.log('on log out ', user)
-		isLoggedIn.set(false)
+		isLoggedIn.set(user)
 	})
 }

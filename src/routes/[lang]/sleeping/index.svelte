0a3d04be225f9135875141db074317a0031ec6f8
Lisa <halvorsen.lisa@gmail.com>
2022-03-07 21:58:22 +0100

    Add login check

--- src/routes/[lang]/sleeping/index.svelte
+++ src/routes/[lang]/sleeping/index.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { translate } from '$lib/translate'
+	import { isLoggedIn } from '$lib/stores/auth'
 
 	export const prerender = true
 </script>
 
-<h1 class="m-t__large color-primary">{translate('accommodation')}</h1>
+{#if $isLoggedIn}
+	<h1 class="m-t__large color-primary">{translate('accommodation')}</h1>
 
-{@html translate('accommodationText')}
+	{@html translate('accommodationText')}
+{/if}

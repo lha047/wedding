e0f200f1318710a1bbe156fa06d19d341414ea87
Lisa Halvorsen <kisansimhae@gmail.com>
2022-03-20 21:53:49 +0100

    remove log

--- src/routes/[lang]/program/index.svelte
+++ src/routes/[lang]/program/index.svelte
@@ -1,7 +1,7 @@
 <script>
 	import WeekendInfo from '$lib/components/WeekendInfo.svelte'
 	import {isLoggedInNetlify} from '$lib/stores/netlifyStore'
-	$: console.log('program', $isLoggedIn)
+
 </script>
 
 {#if $isLoggedInNetlify}
